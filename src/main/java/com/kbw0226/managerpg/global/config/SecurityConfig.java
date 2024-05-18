036e0d228f70684feb902273dba4eacd32934fc4
peace0096 <peace0096@naver.com>
2024-05-18 21:26:48 +0900

    [설정] 기본 로그인 페이지 임시 제거

---
+++ src/main/java/com/kbw0226/managerpg/global/config/SecurityConfig.java
@@ -0,0 +1,21 @@
+package com.kbw0226.managerpg.global.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .httpBasic(AbstractHttpConfigurer::disable);
+
+        return httpSecurity.build();
+    }
+}
