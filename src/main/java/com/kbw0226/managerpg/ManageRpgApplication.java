a1480d3a088e658ad84aee5b05d4f6a2e1a4d0ae
peace0096 <peace0096@naver.com>
2024-05-18 19:49:16 +0900

    [설정] 초기 세팅

---
+++ src/main/java/com/kbw0226/managerpg/ManageRpgApplication.java
@@ -0,0 +1,13 @@
+package com.kbw0226.managerpg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManageRpgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManageRpgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kbw0226/managerpg/ManageRpgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kbw0226.managerpg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManageRpgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
